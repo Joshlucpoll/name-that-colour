e4723313b4db12b4253bb567fe03909285afd2ce
Josh Pollard <49789046+Joshlucpoll@users.noreply.github.com>
2020-08-03 02:03:03 +0100

    Updates

---
+++ precache-manifest.3b74025941f65665ab6952a88331ad50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f65a90a15840adc9605414333eb6eaf",
+    "url": "/name-that-colour/index.html"
+  },
+  {
+    "revision": "5ea5b87e5869273afbe7",
+    "url": "/name-that-colour/static/css/main.d8c00ae0.chunk.css"
+  },
+  {
+    "revision": "db4898690402cdb8bb19",
+    "url": "/name-that-colour/static/js/2.ac8a3e3e.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/name-that-colour/static/js/2.ac8a3e3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ea5b87e5869273afbe7",
+    "url": "/name-that-colour/static/js/main.6f165441.chunk.js"
+  },
+  {
+    "revision": "15b792594af15d888f70",
+    "url": "/name-that-colour/static/js/runtime-main.560e7cf2.js"
+  }
+]);
